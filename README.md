f0812c8fb6bf220bce8f7ca7e4f775148a4729c7
teyimasobo@gmail.com <teyimasobo@gmail.com>
2021-04-04 01:32:11 +0100

    added tap funtionality

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Gsoc-Starterbug-milestone1
-A simple GSoC21 starter bug PWA application with 3 tabs, and is instalable
+A simple GSoC21 starter bug PWA application with 3 tabs, and is installable
+
---
+++ main.js
@@ -0,0 +1,49 @@
+const tap1=document.getElementById('tap1');
+const tap2=document.getElementById('tap2');
+const tap3=document.getElementById('tap3');
+const page1=document.getElementById('page1');
+const page2=document.getElementById('page2');
+const page3=document.getElementById('page3');
+
+const updateTapState=(tap)=>{
+switch (tap) {
+    case 'tap1':
+        tap1.classList.toggle('active');
+        tap2.classList.remove('active');
+        tap3.classList.remove('active');
+        page1.classList.toggle('hide');
+        page2.classList.add('hide');
+        page3.classList.add('hide');
+        break;
+    case 'tap2':
+        tap2.classList.toggle('active');
+        tap1.classList.remove('active');
+        tap3.classList.remove('active');
+        page2.classList.toggle('hide');
+        page1.classList.add('hide');
+        page3.classList.add('hide');
+        break;
+    case 'tap3':
+        tap3.classList.toggle('active');
+        tap1.classList.remove('active');
+        tap2.classList.remove('active');
+        page3.classList.toggle('hide');
+        page2.classList.add('hide');
+        page1.classList.add('hide');
+        break;
+
+    default:
+        break;
+}
+}
+tap1.addEventListener('click',()=>{
+    updateTapState('tap1')
+})
+tap2.addEventListener('click',()=>{
+    updateTapState('tap2')
+})
+
+tap3.addEventListener('click',()=>{
+    updateTapState('tap3')
+})
+
